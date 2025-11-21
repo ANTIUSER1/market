847897849d0b4facecf70b51ddb6ce5b817aabe2
PNS <solovjov_pn@mail.ru>
2025-11-21 15:49:45 +0300

    start OrderController detect

---
+++ src/main/java/pn/market/controllers/OrderController.java
@@ -0,0 +1,11 @@
+package pn.market.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/order")
+@Slf4j
+public class OrderController {
+}
