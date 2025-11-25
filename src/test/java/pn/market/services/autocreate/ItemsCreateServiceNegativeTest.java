e80e89153c62727e8374a22869aa8ae374af2e8b
PNS <solovjov_pn@mail.ru>
2025-11-25 12:04:12 +0300

    done itemcreateservicetest

---
+++ src/test/java/pn/market/services/autocreate/ItemsCreateServiceNegativeTest.java
@@ -0,0 +1,40 @@
+package pn.market.services.autocreate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import pn.market.entities.Item;
+import pn.market.repo.ItemRepo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class ItemsCreateServiceNegativeTest {
+
+    @MockitoBean
+    private ItemRepo itemRepo;
+    @MockitoBean
+    private ItemsCreateService itemsService;
+    private List<Item> items;
+
+
+    @BeforeEach
+    void init() {
+        items = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            items.add(new Item());
+        }
+    }
+
+    @Test
+    void autoCreate() {
+        when(itemRepo.getAllItemsSortedAscById()).thenReturn(items);
+        when(itemsService.autoCreate()).thenReturn(items);
+        assertNotEquals(20, itemsService.autoCreate().size());
+    }
+}
