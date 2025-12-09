bfd7dc61e6a247ee1743671119ef01b0d238cc9c
PNS <solovjov_pn@mail.ru>
2025-12-09 11:51:00 +0300

    remake ordet template ---DONE remake getOrderById method in OrderController  ----- step 4--finish

--- src/main/java/pn/market/controllers/OrderController.java
+++ src/main/java/pn/market/controllers/OrderController.java
@@ -45,18 +45,9 @@ public class OrderController {
             @RequestParam(value = "newOrder") boolean newOrder
     ) {
         Optional<Order> order = orderService.getById(id);
-        System.out.println("    newOrder = " + newOrder);
-        System.out.println("    order present  = " + order.isPresent());
-
-
         if (order.isPresent()) {
-
             OrderContainer orderContainer = orderContainerService.create(order.get());
-
-            System.out.println("    ORDER    = " + order);
-            System.out.println("    ORDER Container    = " + orderContainer);
-            System.out.println("    newOrder = " + newOrder);
-         //   model.addAttribute("order", order.get());
+            model.addAttribute("orderData", orderContainer);
             return "order";
         }
         return "items";
