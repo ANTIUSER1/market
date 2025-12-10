3d2ff6b2bc163c8edb5d51dea72508e8273a92e2
PNS <solovjov_pn@mail.ru>
2025-12-10 16:06:21 +0300

    webflux dependency add---test correction with get

--- src/main/java/pn/market/controllers/CartController.java
+++ src/main/java/pn/market/controllers/CartController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,7 +27,7 @@ public class CartController {
     @Autowired
     private ItemServiceImpl itemService;
 
-    @PostMapping("/items")
+    @GetMapping("/items")
     public String addItem(
             @RequestParam("itemId") Long itemId,
             @RequestParam("action") String action,
