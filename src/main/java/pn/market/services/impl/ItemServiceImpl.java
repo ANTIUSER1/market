4e3a775962e9406503d585eac581b6aa00bfbc68
PNS <solovjov_pn@mail.ru>
2025-12-13 12:40:54 +0300

    --step 4-c  pluz&minuz into cart--- for any item --- do not refactored ---remove  redundant commented blocks in ItemServiceImpl

--- src/main/java/pn/market/services/impl/ItemServiceImpl.java
+++ src/main/java/pn/market/services/impl/ItemServiceImpl.java
@@ -154,7 +154,7 @@ public class ItemServiceImpl implements TService<Item> {
         return imgPath;
     }
 
-    public Mono<Item> save(Item i) {
-        return itemRepo.save(i);
-    }
+//    public Mono<Item> save(Item i) {
+//        return itemRepo.save(i);
+//    }
 }
