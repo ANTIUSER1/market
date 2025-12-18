0aec92bbae9fed9587467f20aaea4255b37c41f3
PNS <solovjov_pn@mail.ru>
2025-12-18 22:15:26 +0300

    --step  4 working with tests  --T    carterviceImpl/test  --  finish

--- src/test/java/pn/market/services/impl/CartServiceImplNegativeTest.java
+++ src/test/java/pn/market/services/impl/CartServiceImplNegativeTest.java
@@ -2,28 +2,29 @@ package pn.market.services.impl;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import pn.market.entities.Cart;
 import pn.market.entities.Item;
 import pn.market.repo.CartRepo;
 import pn.market.repo.ItemRepo;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class CartServiceImplNegativeTest {
 
-    @MockitoBean
+    @Mock
     private CartRepo cartRepo;
-    @MockitoBean
+    @Mock
     private ItemRepo itemRepo;
     @Autowired
     private ItemServiceImpl itemService;
-    @MockitoBean
+    @Mock
     private CartServiceImpl cartService;
 
     private Cart cart;
@@ -36,30 +37,36 @@ class CartServiceImplNegativeTest {
         cart.setId(1L);
     }
 
+
+    @Test
+    void getByIdTest() {
+        when(cartService.getById(1L)).thenReturn(Mono.empty());
+        assertNotEquals(Flux.empty(), cartService.getById(1L));
+    }
+
     @Test
     void findAllAndPagingTest() {
         when(cartService.findAllAndPaging(Mono.empty())).thenReturn(Mono.empty());
-        assertEquals(Mono.empty(), cartService.findAllAndPaging(Mono.empty()));
+        assertNotEquals(Flux.empty(), cartService.findAllAndPaging(Mono.empty()));
     }
 
     @Test
     void createNewCartTest() {
         when(cartRepo.save(cart)).thenReturn(Mono.empty());
         when(cartService.createNewCart()).thenReturn(Mono.empty());
-        assertEquals(Mono.empty(), cartService.createNewCart());
+        assertNotEquals(Flux.empty(), cartService.createNewCart());
     }
 
     @Test
     void createCartForItemIfNotExistsTest() {
-        when(itemService.findById(1L)).thenReturn(Mono.empty());
         when(cartService.createCartForItemIfNotExists(1L)).thenReturn(Mono.empty());
-        assertEquals(Mono.empty(), cartService.createCartForItemIfNotExists(1L));
+        assertNotEquals(Flux.empty(), cartService.createCartForItemIfNotExists(1L));
     }
 
     @Test
     void placeItemToCartTest() {
-        when(itemService.findById(1L)).thenReturn(Mono.empty());
         when(cartService.placeItemToCart(1L, "1L")).thenReturn(Mono.empty());
-        assertEquals(Mono.empty(), cartService.placeItemToCart(1L, "1L"));
+        assertNotEquals(Flux.empty(), cartService.placeItemToCart(1L, "1L"));
     }
+
 }
