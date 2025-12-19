047e0cbe2ecb53416000ea1d1401d41b9d15aa85
PNS <solovjov_pn@mail.ru>
2025-12-19 17:14:33 +0300

    --step  6 OrderControllerNegativeTestTest done

--- src/test/java/pn/market/controllers/OrderControllerNegativeTest.java
+++ src/test/java/pn/market/controllers/OrderControllerNegativeTest.java
@@ -1,48 +1,63 @@
 package pn.market.controllers;
 
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import pn.market.services.impl.ItemServiceImpl;
+import pn.market.services.impl.OrderServiceImpl;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
-@WebMvcTest(OrderController.class)
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@WebFluxTest(OrderController.class)
+@Import({OrderServiceImpl.class, ItemServiceImpl.class})
 class OrderControllerNegativeTest {
-/*
+
     @MockitoBean
     private OrderServiceImpl orderService;
 
+    @MockitoBean
+    private ItemServiceImpl itemService;
+
+
     @Autowired
-    private MockMvc mvc;
+    private WebTestClient webTestClient;
 
-    private Order order;
-
-    @BeforeEach
-    void init() {
-        order = new Order();
-        order.setId(1L);
+    @Test
+    void allOrders() {
+        Mockito.when(orderService.findAll())
+                .thenReturn(Flux.empty());
+        webTestClient.get().uri("/orders")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.TEXT_HTML)
+                .expectBody(String.class)
+                .consumeWith(response -> {
+                    String body = response.getResponseBody();
+                    assertNotNull(body);
+                    assertTrue(body.contains("<html")); // Проверяем, что страница содержит форму
+                });
     }
 
     @Test
-    void asdOrders() throws Exception {
-        when(orderService.findAllOrders()).thenReturn(new ArrayList<>());
-        mvc.perform(MockMvcRequestBuilders.get("/ords"))
-                .andExpect(status().is4xxClientError());
+    void getOrderById() {
+        Mockito.when(orderService.getById(1000L)).thenReturn(Mono.empty());
+        webTestClient.get().uri("/orders/{id}", 100L)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.TEXT_HTML)
+                .expectBody(String.class)
+                .consumeWith(response -> {
+                    String body = response.getResponseBody();
+                    assertNotNull(body);
+                    assertTrue(body.contains("<html")); // Проверяем, что страница содержит форму
+                });
     }
-
-    @Test
-    void getOrderById() throws Exception {
-        when(orderService.getById(1L)).thenReturn(Optional.ofNullable(order));
-        assertFalse(orderService.getById(1L).isEmpty());
-        mvc.perform(MockMvcRequestBuilders.get("/ordes/1")
-                        .param("id", String.valueOf(1))
-                        .param("newOrder", "true"))
-                .andExpect(status().is4xxClientError());
-
-    }
-
-    @Test
-    void buyOrder() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.post("/orders/buyyy/3")
-                        .param("id", String.valueOf(1)))
-                .andExpect(status().is4xxClientError());
-    }
-
- */
 }
