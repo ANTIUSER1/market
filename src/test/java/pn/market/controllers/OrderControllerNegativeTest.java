049eb8a08a99e60cf9c3b5226cd8396a798cb2d8
PNS <solovjov_pn@mail.ru>
2025-12-19 17:17:05 +0300

    --step  6 OrderControllerNegativeTestTest done

--- src/test/java/pn/market/controllers/OrderControllerNegativeTest.java
+++ src/test/java/pn/market/controllers/OrderControllerNegativeTest.java
@@ -5,16 +5,12 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import pn.market.services.impl.ItemServiceImpl;
 import pn.market.services.impl.OrderServiceImpl;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @WebFluxTest(OrderController.class)
 @Import({OrderServiceImpl.class, ItemServiceImpl.class})
@@ -34,30 +30,16 @@ class OrderControllerNegativeTest {
     void allOrders() {
         Mockito.when(orderService.findAll())
                 .thenReturn(Flux.empty());
-        webTestClient.get().uri("/orders")
+        webTestClient.get().uri("/orders/oo")
                 .exchange()
-                .expectStatus().isOk()
-                .expectHeader().contentType(MediaType.TEXT_HTML)
-                .expectBody(String.class)
-                .consumeWith(response -> {
-                    String body = response.getResponseBody();
-                    assertNotNull(body);
-                    assertTrue(body.contains("<html")); // Проверяем, что страница содержит форму
-                });
+                .expectStatus().is4xxClientError();
     }
 
     @Test
     void getOrderById() {
         Mockito.when(orderService.getById(1000L)).thenReturn(Mono.empty());
-        webTestClient.get().uri("/orders/{id}", 100L)
+        webTestClient.get().uri("/orders/oo{id}", 100L)
                 .exchange()
-                .expectStatus().isOk()
-                .expectHeader().contentType(MediaType.TEXT_HTML)
-                .expectBody(String.class)
-                .consumeWith(response -> {
-                    String body = response.getResponseBody();
-                    assertNotNull(body);
-                    assertTrue(body.contains("<html")); // Проверяем, что страница содержит форму
-                });
+                .expectStatus().is4xxClientError();
     }
 }
