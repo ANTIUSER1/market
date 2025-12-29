f824d012151836f3163e7f139102421587874039
PNS <solovjov_pn@mail.ru>
2025-12-29 18:03:52 +0300

    step 1-b Payment  ---  try one test-user in db

--- payment/src/main/java/pn/payment/services/UserService.java
+++ payment/src/main/java/pn/payment/services/UserService.java
@@ -1,17 +1,22 @@
 package pn.payment.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webflux.autoconfigure.WebFluxProperties;
 import org.springframework.stereotype.Service;
 import pn.payment.ent.User;
+import pn.payment.repo.UserRepo;
 import reactor.core.publisher.Mono;
 
 @Service
 public class UserService {
 
 
+    @Autowired
+    private UserRepo repo;
+
     public Mono<User> create(){
         User result=new User("U","p",   100_000L)    ;
-        return Mono.just(result);
+        return repo.save(result);
 
     }
 }
