a6bebc4d123d5dc95509e73dc1faee18c0811565
PNS <solovjov_pn@mail.ru>
2026-02-09 16:03:15 +0300

    start create backend server fo VITRO Resources

---
+++ market-vitro-back/src/test/java/pn/market/vitroBack/MarketVitroBackApplicationTests.java
@@ -0,0 +1,13 @@
+package pn.market.vitroBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketVitroBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
