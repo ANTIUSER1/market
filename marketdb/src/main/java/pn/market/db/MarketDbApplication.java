2d07295e38770cda4459876f7e8ec5c61cf5448d
PNS <solovjov_pn@mail.ru>
2026-02-09 11:15:45 +0300

    add module market-db

---
+++ marketdb/src/main/java/pn/market/db/MarketDbApplication.java
@@ -0,0 +1,13 @@
+package pn.market.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketDbApplication.class, args);
+	}
+
+}
