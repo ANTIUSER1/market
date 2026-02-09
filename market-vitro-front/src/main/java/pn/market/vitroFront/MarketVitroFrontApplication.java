fe1eb9633f78e78db60ec8a85f92be3cda8afb61
PNS <solovjov_pn@mail.ru>
2026-02-09 16:54:57 +0300

    start create front-server

---
+++ market-vitro-front/src/main/java/pn/market/vitroFront/MarketVitroFrontApplication.java
@@ -0,0 +1,13 @@
+package pn.market.vitroFront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketVitroFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketVitroFrontApplication.class, args);
+	}
+
+}
