08cb8e8bb28a3400c1726a8f2e1e5497ac4cce64
PNS <solovjov_pn@mail.ru>
2026-02-09 11:19:04 +0300

    remove module market-entities

---
+++ market-entities/src/test/java/pn/market/market_entities/MarketEntitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package pn.market.market_entities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketEntitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
