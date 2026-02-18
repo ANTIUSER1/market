b13410635d9cb2db0ecc900f8b0f892d78564aa8
PNS <solovjov_pn@mail.ru>
2026-02-18 16:26:50 +0300

    place itim to cart of user---server-front

--- market-vitro-front/src/main/java/pn/market/vitroFront/controllers/CartController.java
+++ market-vitro-front/src/main/java/pn/market/vitroFront/controllers/CartController.java
@@ -16,6 +16,8 @@ import pn.market.vitroFront.servicies.LoginService;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 import static pn.market.vitroFront.config.AuthPaths.VITRO_CART_API;
 
 @Controller
@@ -67,12 +69,16 @@ public class CartController {
         return r;
     }
 
-    @GetMapping("/add-item-to-cart/{itemId}")
-    public Mono<String> additemsList(@PathVariable("itemId") long itemId) {
+    @GetMapping("/add-item-to-cart-of-user/{itemId}")
+    public Mono<String> additemsList(
+            @PathVariable("itemId") long itemId
+    ) {
+        Long userId = loginService.getUserData().getId();
+        AtomicReference<Long> cartId = new AtomicReference<>(0L);
         webClient.get()
-                .uri(VITRO_CART_API + "/create/" + itemId)
-                .retrieve().bodyToMono(Cart.class).subscribe();
-        return Mono.just("redirect:/cart/3");
+                .uri(VITRO_CART_API + "/create/" + userId + "/" + itemId)
+                .retrieve().bodyToMono(Cart.class).subscribe(c -> cartId.set(c.getId()));
+        return Mono.just("redirect:/cart/" + cartId.get());
     }
 
     @GetMapping("/items")
