ac6f1ffbad18399c51e8de1e3aacf7f9e69da554
PNS <solovjov_pn@mail.ru>
2026-02-23 18:46:56 +0300

    Flux<CartItems> findByItemId

--- market-vitro-back/src/main/java/pn/market/vitroBack/repo/CartItemsRepo.java
+++ market-vitro-back/src/main/java/pn/market/vitroBack/repo/CartItemsRepo.java
@@ -11,6 +11,9 @@ public interface CartItemsRepo extends ReactiveCrudRepository<CartItems, Long> {
     @Query("SELECT * FROM carts_items ci WHERE ci.cart_id = $1    ")
     Flux<CartItems> findByCartId(Long cartId);
 
+    @Query("SELECT * FROM carts_items ci WHERE ci.item_id = $1    ")
+    Flux<CartItems> findByItemId(Long item_id);
+
     @Query("SELECT COUNT(*) FROM carts_items ci WHERE ci.item_id = $1 AND  ci.cart_id = $2    ")
-    Mono<Long> countOfCartAndItemIdId(Long itemId, Long cartId);
+    Mono<Long> countOfCartAndItemId(Long itemId, Long cartId);
 }
