deb739405fd1f9ffd2e11dfcb005e26cf41b3a05
PNS <solovjov_pn@mail.ru>
2026-02-26 19:59:30 +0300

    BACK--start optimize --- remove redundant  p-4-3  ---- CART TESTED

--- market-vitro-back/src/main/java/pn/market/vitroBack/servicies/impl/CartRemoveUtilityService.java
+++ market-vitro-back/src/main/java/pn/market/vitroBack/servicies/impl/CartRemoveUtilityService.java
@@ -17,25 +17,23 @@ public class CartRemoveUtilityService {
     private CartControlUtilityService cartControlUtilityService;
 
     public Mono<Item> removeItemFromCartOfUser(Mono<List<CartItems>> cartItemsListMono, Mono<Item> itemMono, Long userId) {
-
         return Mono.zip(cartItemsListMono, itemMono)
                 .map(t -> {
                     Item i = t.getT2();
                     System.out.println("***------------REMOVED ITEM " + i);
                     List<CartItems> cartItemsList = t.getT1();
                     List<CartItems> cartItemsListTMP = new ArrayList<>();
-                    //       System.out.println("      CART-LIST-SIZE -- "+cartItemsList.size());
+
                     for (CartItems cii : cartItemsList) {
                         if (i.getId() == cii.getItemId()) {
-                            System.out.println("     ITIM     " + i + "    " + cii);
                             cartItemsListTMP.add(cii);
                         }
                     }
+
                     if (!cartItemsListTMP.isEmpty()) {
                         CartItems ci = cartItemsListTMP.get(0);
-                        System.out.println("REMOVING      " + ci + "   " + ci.getId() + "  /  " + ci.getItemId());
                         cartControlUtilityService.deleteCartItemsById(ci.getId()).subscribe();
-                        System.out.println(" CI TO REMOVR  " + ci + " \nITEM " + i);
+
                         Mono<Item> countMono = cartControlUtilityService.countOfCartAndItemId(i.getId(), ci.getCartId())
                                 .map(totalCount -> {
                                     long cnt = totalCount - 1;
@@ -46,11 +44,9 @@ public class CartRemoveUtilityService {
                                         i.setCount(0L);
                                     }
                                     cartControlUtilityService.saveItem(i).subscribe();
-
                                     return i;
                                 });
                         countMono.subscribe();
-
                     }
                     return i;
                 });
