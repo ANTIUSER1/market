d94b1d50fd368a9f040a23796a45f8e40f63f98b
PNS <solovjov_pn@mail.ru>
2026-02-26 14:19:38 +0300

    Change PersonData==========>>>>UserData

--- market-payment/src/main/java/pn/payment/repo/PersonDataRepo.java
+++ market-payment/src/main/java/pn/payment/repo/PersonDataRepo.java
@@ -4,4 +4,5 @@ import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import pn.market.market_entities.forPAYMENTS.PersonData;
 
 public interface PersonDataRepo extends ReactiveCrudRepository<PersonData, Long> {
+
 }
