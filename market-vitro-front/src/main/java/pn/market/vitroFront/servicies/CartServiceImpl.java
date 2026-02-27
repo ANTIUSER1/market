5e942382771bc9fc2d913fe2c0df63ee30d295ee
PNS <solovjov_pn@mail.ru>
2026-02-27 11:16:49 +0300

    front--start -- optimize code CartServiceImpl  service  e -7

--- market-vitro-front/src/main/java/pn/market/vitroFront/servicies/CartServiceImpl.java
+++ market-vitro-front/src/main/java/pn/market/vitroFront/servicies/CartServiceImpl.java
@@ -46,38 +46,6 @@ public class CartServiceImpl implements TService<Cart> {
                 .bodyToMono(Item.class);
 
     }
-/*
-    public Mono<Item> placeItemToCart(
-            Long itemId,
-            String action) {
-        System.out.println(".........PLACE!!!  " + itemId + "    " + action);
-        System.out.println(".........ITEM GET FROM    " + "/api/vitro/items/i/" + itemId);
-        Mono<Item> itemMono = itemById(itemId)
-                .map(i -> {
-                            if (i.getCartId() == null) {
-                                webClient.get()
-                                        .uri(VITRO_CART_API + "/create/" + itemId)
-                                        .retrieve().bodyToMono(Cart.class)
-                                        .map(cc -> {
-                                                    System.out.println("    ^^^^^   CCC " + cc.getId());
-                                                    i.setCartId(cc.getId());
-                                                    return cc;
-                                                }
-                                        )
-                                        .subscribe();
-                            }
-                            System.out.println("   ::::IIIIIII--CID:: " + i.getCartId());
-                            return i;
-                        }
-                );
-        itemMono.subscribe(i -> System.out.println("  RE-REQUEST III \n" + i));
-        itemMono.subscribe(imm -> System.out.println("----IMM CART " + imm.getCartId()));
-        itemMono.subscribe(imm -> System.out.println("----IMM ID " + imm.getId()));
-        System.out.println("000000000000000000000000");
-        itemMono = addItemToCart(itemMono, itemId, action);
-        return itemMono;
-    }
-    */
 
     public Mono<Item> placeItemToCartOfUser(
             Long userId, Long itemId,
@@ -93,31 +61,6 @@ public class CartServiceImpl implements TService<Cart> {
                     .retrieve().bodyToMono(Item.class);
             return cartMono.map(c -> itemById(itemId)).flatMap(i -> i);
         }
-        return Mono.just(new Item());
-    }
-
-    private Mono<Item> addItemToCart(Mono<Item> itemMono, Long itemId, String action) {
-/*
-        System.out.println("     _____00000-itemId- " + itemId);
-        System.out.println("     _____00000-action- " + action);
-        return Mono.zip(itemMono, longMono
-                        // this.createCartForItemIfNotExists(itemMono, itemId)
-                )
-                .map(t -> {
-                    System.out.println("   IN MAP RUN@");
-                    Item i = t.getT1();
-                    Long cartId = t.getT2();
-                    System.out.println("    ---CART-ID::::: " + cartId);
-                    if (action != null && cartId != null) {
-                        return webClient.get()
-                                .uri(VITRO_ITEM_API + "/add-cart/"
-                                        + i.getId() + "/" + cartId + "/" + action)
-                                .retrieve().bodyToMono(Item.class);
-                    }
-                    return Mono.just(i);
-                })
-                .flatMap(i -> i);
-*/
         return Mono.just(new Item());
     }
 
