7f61fabd4f032c1df852c9c3205f064ed49c4662
PNS <solovjov_pn@mail.ru>
2026-03-02 16:41:26 +0300

    ItemRepoTest done

---
+++ market-vitro-back/src/test/java/pn/market/vitroBack/repo/ItemRepoTest.java
@@ -0,0 +1,71 @@
+package pn.market.vitroBack.repo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.r2dbc.core.DatabaseClient;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import pn.market.market_entities.forWEB.Item;
+import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@SpringBootTest
+class ItemRepoTest {
+
+
+    private final List<Item> items = new ArrayList<>();
+    private Item item;
+    private Flux<Item> itemsFlux = null;
+    private Pageable pageable;
+    @MockitoBean
+    private CartRepo cartRepo;
+    @MockitoBean
+    private OrderRepo orderRepo;
+    @MockitoBean
+    private ItemRepo itemRepo;
+
+    @Autowired
+    private DatabaseClient databaseClient;
+
+    @BeforeEach
+    void init() {
+        item = new Item();
+        item.setId(1L);
+        for (int i = 1; i < 10; i++) {
+            Item item = new Item();
+            item.setId((long) i);
+            items.add(item);
+        }
+        itemsFlux = Flux.fromIterable(items);
+    }
+
+    @Test
+    void findItemsByUserId() {
+        Mockito.when(itemRepo.findItemsByUserId(1L))
+                .thenReturn(itemsFlux);
+        Assertions.assertEquals(itemsFlux, itemRepo.findItemsByUserId(1L));
+    }
+
+    @Test
+    void findItemsByOrderId() {
+        Mockito.when(itemRepo.findItemsByOrderId(1L))
+                .thenReturn(itemsFlux);
+        Assertions.assertEquals(itemsFlux, itemRepo.findItemsByOrderId(1L));
+    }
+
+
+    @Test
+    void findItemsByCartId() {
+        Mockito.when(itemRepo.findItemsByCartId(1L))
+                .thenReturn(itemsFlux);
+        Assertions.assertEquals(itemsFlux, itemRepo.findItemsByCartId(1L));
+    }
+
+}
