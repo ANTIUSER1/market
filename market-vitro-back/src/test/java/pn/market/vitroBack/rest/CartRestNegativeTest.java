e280928ea4fe67d5c7a59e2c2ba83352175abe95
PNS <solovjov_pn@mail.ru>
2026-03-02 20:04:12 +0300

    CartRestTest + negative done

---
+++ market-vitro-back/src/test/java/pn/market/vitroBack/rest/CartRestNegativeTest.java
@@ -0,0 +1,51 @@
+package pn.market.vitroBack.rest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import pn.market.market_entities.forWEB.Cart;
+import pn.market.market_entities.forWEB.Item;
+import pn.market.vitroBack.servicies.impl.CartServiceImpl;
+import reactor.core.publisher.Mono;
+
+@SpringBootTest
+class CartRestNegativeTest {
+
+    @MockitoBean
+    private CartServiceImpl cartService;
+
+    private Mono<Item> itemMono;
+    private Mono<Cart> cartMono;
+    private Mono<Long> longMono;
+
+    @BeforeEach
+    void init() {
+        itemMono = Mono.just(new Item());
+        cartMono = Mono.just(new Cart());
+        longMono = Mono.just(100L);
+    }
+
+    @Test
+    void createCartOfUser() {
+        Mockito.when(cartService.createOrUseCartOfUser(1L, 1L))
+                .thenReturn(cartMono);
+        Assertions.assertNotEquals(Mono.empty(), cartService.createOrUseCartOfUser(1L, 1L));
+    }
+
+    @Test
+    void getTotalSumOfCart() {
+        Mockito.when(cartService.getTotalSumOfCart(1L))
+                .thenReturn(longMono);
+        Assertions.assertNotEquals(Mono.empty(), cartService.getTotalSumOfCart(1L));
+    }
+
+    @Test
+    void removeCartOfUser() {
+        Mockito.when(cartService.removeFromCartOfUser(1L, 1L))
+                .thenReturn(itemMono);
+        Assertions.assertNotEquals(Mono.empty(), cartService.removeFromCartOfUser(1L, 1L));
+    }
+}
