55d9c4cad27245c68134d48c65ad44b2d297154f
PNS <solovjov_pn@mail.ru>
2026-03-02 18:01:19 +0300

    CartItemsRepoTest + negative done

---
+++ market-vitro-back/src/test/java/pn/market/vitroBack/repo/CartRepoNegativeTest.java
@@ -0,0 +1,34 @@
+package pn.market.vitroBack.repo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import pn.market.market_entities.forWEB.Cart;
+import reactor.core.publisher.Mono;
+
+@SpringBootTest
+public class CartRepoNegativeTest {
+
+
+    private Cart cart;
+    private Mono<Cart> cartMono;
+    @MockitoBean
+    private CartRepo cartRepo;
+
+
+    @BeforeEach
+    void init() {
+        cart = new Cart();
+        cartMono = Mono.just(cart);
+    }
+
+    @Test
+    void findByUserId() {
+        Mockito.when(cartRepo.findById(1L))
+                .thenReturn(cartMono);
+        Assertions.assertNotEquals(Mono.empty(), cartRepo.findById(1L));
+    }
+}
