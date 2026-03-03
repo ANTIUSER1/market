847e103de229ab899df6141996e79bbf0ed78883
PNS <solovjov_pn@mail.ru>
2026-03-03 13:43:34 +0300

    CartRemoveUtilityServiceTest + negative done+

--- market-vitro-back/src/test/java/pn/market/vitroBack/servicies/impl/CartRemoveUtilityServiceTest.java
+++ market-vitro-back/src/test/java/pn/market/vitroBack/servicies/impl/CartRemoveUtilityServiceTest.java
@@ -6,11 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import pn.market.market_entities.forWEB.CartItems;
-import pn.market.market_entities.forWEB.Item;
 import reactor.core.publisher.Mono;
-
-import java.util.List;
 
 @SpringBootTest
 class CartRemoveUtilityServiceTest {
@@ -19,9 +15,7 @@ class CartRemoveUtilityServiceTest {
     private CartControlUtilityService cartControlUtilityService;
 
     private Mono<Void> voidMono;
-    private Mono<List<CartItems>> cartItemsListMono;
-    private Mono<Item> itemMono;
-    private Long userId;
+
 
     @BeforeEach
     void init() {
