1f0695d6d1afb477fc31851bc56a020178e531e7
PNS <solovjov_pn@mail.ru>
2026-03-11 19:47:17 +0300

    PaymentServiceImplTest done

--- market-vitro-front/src/test/java/pn/market/vitroFront/servicies/PaymentsServiceImplTest.java
+++ market-vitro-front/src/test/java/pn/market/vitroFront/servicies/PaymentsServiceImplTest.java
@@ -3,12 +3,9 @@ package pn.market.vitroFront.servicies;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import reactor.core.publisher.Mono;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class PaymentsServiceImplTest {
@@ -20,6 +17,6 @@ class PaymentsServiceImplTest {
     void sendPaymentInfo() {
         Mockito.when(paymentSupplier.get())
                 .thenReturn(Mono.just("GETT"));
-        Assertions.assertEquals(Mono.just("GETT").block(),paymentSupplier.get().block() );
+        Assertions.assertEquals(Mono.just("GETT").block(), paymentSupplier.get().block());
     }
 }
